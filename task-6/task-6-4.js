c008495aa6302b7e8ed4208a3d59bb434b6a884a
Artem-Zuborev <zuborev_artem@mail.ru>
2020-12-11 21:25:29 +0300

    task-6-4

--- task-6/task-6-4.js
+++ task-6/task-6-4.js
@@ -1,6 +1,6 @@
 function multiplyAll(mas) {
     return function (n) {
-        let newMas = []
+        const newMas = []
         for (let i = 0; i < mas.length; i++) {
             newMas.push(mas[i] * n);
         }
