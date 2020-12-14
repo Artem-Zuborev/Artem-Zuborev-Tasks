8696958d3c195fc473d4311dcc0ed67e4156e79c
Artem-Zuborev <zuborev_artem@mail.ru>
2020-12-14 18:52:30 +0300

    rename variable

--- task-5/task-5-1.js
+++ task-5/task-5-1.js
@@ -1,9 +1,9 @@
 function calculateString(str) {
     if (typeof str === 'string') {
-        let mas = str.split(' ');
-        let newMas = [];
+        const mas = str.split(' ');
+        const newMas = [];
         for (let i = 0; i < mas.length; i++) {
-            let result = mas[i] + ' ' + mas[i].length;
+            const result = mas[i] + ' ' + mas[i].length;
             newMas.push(result)
         }
         console.log(newMas);
