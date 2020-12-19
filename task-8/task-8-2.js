480b3f43a4c6ff72d46ce8d90ede8c1c1d5e8510
Artem-Zuborev <zuborev_artem@mail.ru>
2020-12-19 20:32:33 +0300

    task-8-2

---
+++ task-8/task-8-2.js
@@ -0,0 +1,28 @@
+//Реализовать quickSort алгоритм (через рекурсию)
+// В качестве сортировки используйте массив чисел
+
+function quicksort(mas) {
+    if (mas.length <= 1) {
+        return mas;
+    }
+
+    const pivot = mas[0];
+
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < mas.length; i++) {
+        if (mas[i] < pivot) {
+            left.push(mas[i]);
+        } else {
+            right.push(mas[i]);
+        }
+    }
+
+    return quicksort(left).concat(pivot, quicksort(right));
+};
+
+var mas = [23, 45, 16, 37, 3, 99, 22];
+var newSortedMas = quicksort(mas);
+
+console.log('Sorted array', newSortedMas);
