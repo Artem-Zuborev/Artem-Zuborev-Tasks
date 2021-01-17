b09edd155ee77cd9c72f306e5a8b2ce99b15359f
Artem-Zuborev <zuborev_artem@mail.ru>
2021-01-17 23:04:02 +0300

    add catch

--- task-9/script/weather.js
+++ task-9/script/weather.js
@@ -12,11 +12,9 @@ const closeIconWeather = document.querySelector('.close-icon-weather');
 openWeatherBtn.addEventListener('click', function (e) {
     weather.classList.add('show-container-weather');
 });
-// close weather list
 closeIconWeather.addEventListener('click', (e) => {
     weather.classList.remove('show-container-weather')
 })
-
 async function getWeather() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=6b927d86de68c0b3f2e963ce6ccad2f8&units=metric`;
     const res = await fetch(url);
@@ -29,7 +27,9 @@ async function getWeather() {
     speedWind.textContent = 'Скорость ветра: ' + data.wind.speed + ' м/с'
     humidity.textContent = 'Влажность воздуха: ' + data.main.humidity + ' %'
 }
-
+getWeather().catch(e => {
+    console.log(`Error! ${e}`)
+})
 function getCity() {
     if (localStorage.getItem('city-name') === null || localStorage.getItem('city-name') === '') {
         city.textContent = '[Введите название города на латинице]';
@@ -37,8 +37,6 @@ function getCity() {
         city.textContent = localStorage.getItem('city-name');
     }
 }
-
-// Set city
 function setCity(e) {
     if (e.type === 'keypress') {
 
