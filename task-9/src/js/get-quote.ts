81207c529417bfc384ddcd43d181d04660c9c35c
Artem-Zuborev <zuborev_artem@mail.ru>
2021-01-28 00:03:30 +0300

    add type

--- task-9/src/js/get-quote.ts
+++ task-9/src/js/get-quote.ts
@@ -1,8 +1,8 @@
 
-const blockquote = document.querySelector('blockquote');
-const figcaption = document.querySelector('figcaption');
-const btn = document.querySelector('.quote__btn');
-async function getQuote() {
+const blockquote:HTMLElement = document.querySelector('blockquote');
+const figcaption:HTMLElement = document.querySelector('figcaption');
+const btn:HTMLElement = document.querySelector('.quote__btn');
+async function getQuote():Promise<any> {
     const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru`;
     const res = await fetch(url);
     const data = await res.json();
