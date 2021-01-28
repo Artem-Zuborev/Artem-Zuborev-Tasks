d32e2d940803afd82d2fcb8bdb8f744608a923dd
Artem-Zuborev <zuborev_artem@mail.ru>
2021-01-29 01:03:00 +0300

    add try, catch

--- task-9/src/js/get-quote.ts
+++ task-9/src/js/get-quote.ts
@@ -1,16 +1,23 @@
+const blockquote: HTMLElement = document.querySelector('blockquote');
+const figcaption: HTMLElement = document.querySelector('figcaption');
+const btn: HTMLElement = document.querySelector('.quote__btn');
 
-const blockquote:HTMLElement = document.querySelector('blockquote');
-const figcaption:HTMLElement = document.querySelector('figcaption');
-const btn:HTMLElement = document.querySelector('.quote__btn');
-async function getQuote():Promise<any> {
-    const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru`;
-    const res = await fetch(url);
-    const data = await res.json();
+async function getQuote(): Promise<any> {
+    try {
+        const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru`;
+        const res = await fetch(url);
+        const data = await res.json();
+        renderQuote(data)
+    } catch (e) {
+        blockquote.textContent = `Error! ${e}`;
+    }
+}
+
+function renderQuote(data) {
     blockquote.textContent = data.quoteText;
     figcaption.textContent = data.quoteAuthor;
 }
-getQuote().catch(e => {
-    console.log(`Error! ${e}`)
-})
+
+
 document.addEventListener('DOMContentLoaded', getQuote);
 btn.addEventListener('click', getQuote);
