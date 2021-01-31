fc293a7ad63ad3369911ba3af5e51f3cdf55783c
Artem-Zuborev <zuborev_artem@mail.ru>
2021-01-31 23:51:55 +0300

    add

--- task-9/src/js/get-quote.ts
+++ task-9/src/js/get-quote.ts
@@ -2,7 +2,8 @@ const blockquote: HTMLElement = document.querySelector('blockquote');
 const figcaption: HTMLElement = document.querySelector('figcaption');
 const btn: HTMLElement = document.querySelector('.quote__btn');
 
-async function getQuote(): Promise<any> {
+
+async function getQuote(): Promise<void> {
     try {
         const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru`;
         const res = await fetch(url);
