d78844b74de7c4aa03b50e2670316f10249be2d0
Artem-Zuborev <zuborev_artem@mail.ru>
2021-02-21 00:19:36 +0300

    add routing game

--- Pocemons-project/src/app/app-routing.module.ts
+++ Pocemons-project/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {PokemonsListComponent} from './pokemons-list/pokemons-list.component';
 import {AboutPokemonsComponent} from './about-pokemons/about-pokemons.component';
 import {MyPokemonsComponent} from './my-pokemons/my-pokemons.component';
 import {PokemonInfoComponent} from './pokemon-info/pokemon-info.component';
+import {GameComponent} from './game/game.component';
 
 const routes: Routes = [
-  {path: '', component: AboutPokemonsComponent},
+  {path: '', component: GameComponent},
   {path: 'about-pokemons', component: AboutPokemonsComponent},
   {path: 'pokemons-list', component: PokemonsListComponent},
   {path: 'my-pokemons', component: MyPokemonsComponent},
+  {path: 'game', component: GameComponent},
   {path: 'pokemon-info/:id', component: PokemonInfoComponent}
 ];
 
