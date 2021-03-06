63ebc58b2896cb2ef76028598afea3c3b23ab0a5
Artem-Zuborev <zuborev_artem@mail.ru>
2021-03-06 17:07:24 +0300

    add social-bar

--- Pocemons-project/src/app/social-bar/social-bar.component.spec.ts
+++ Pocemons-project/src/app/social-bar/social-bar.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormComponent } from './form.component';
+import { SocialBarComponent } from './social-bar.component';
 
-describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+describe('SocialBarComponent', () => {
+  let component: SocialBarComponent;
+  let fixture: ComponentFixture<SocialBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [ SocialBarComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
+    fixture = TestBed.createComponent(SocialBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
